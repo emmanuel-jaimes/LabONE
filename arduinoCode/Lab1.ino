b89d706892cdd85500f9ffae1ec5e6723980906f
Brendan Suter <brendan-suter@uiowa.edu>
2023-10-01 18:11:30 -0500

    Added Arduino Code

---
+++ arduinoCode/Lab1.ino
@@ -0,0 +1,68 @@
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+LiquidCrystal lcd(12, 11, 6, 5, 4, 3); // RS -> PD12; EN -> PD11; D4 -> PD6; D5 -> PD5; D6 -> PD4; D7 -> PD3
+OneWire oneWire(2); //sensor data plugged into PD2
+DallasTemperature therm(&oneWire);
+
+String celc = "Degrees C: "; // Initialize Strings for LCD
+String fahr = "Degrees F: ";
+String err = "    Error   ";
+String noSens = "No Sensor Found";
+
+void reqTemp();
+void printLCD(String s1, String s2);
+SoftwareSerial BTSerial(9, 10); // Initialize HC-06 on Arduino
+
+void setup(void)
+{
+  Serial.begin(9600);
+  therm.begin();
+  lcd.begin(16,2);
+  BTSerial.begin(9600);
+  
+  pinMode(7, INPUT); // Initialize Pin 7 as button
+  
+  reqTemp(); // Requests temperature
+  
+  celc = "Degrees C: " + String(therm.getTempCByIndex(0));
+  fahr = "Degrees F: " + String(therm.getTempFByIndex(0));
+}
+
+
+void loop() {
+  delay(1000); // Sets one second interval
+  
+  reqTemp();
+  while(digitalRead(7) == HIGH){
+    if(therm.getTempCByIndex(0) == -127){ // Checks if Sensor isn't receiving data
+      printLCD(err, noSens); // Prints "Error No Sensor Found" to LCD
+    }
+    else{
+      fahr = "Degrees F: " + String(therm.getTempFByIndex(0));
+      celc = "Degrees C: " + String(therm.getTempCByIndex(0));
+      
+      printLCD(celc,fahr); // Prints the Celcius and Fahrenheit Temperatures to the LCD
+      Serial.print("\n" + String(therm.getTempCByIndex(0)));
+      BTSerial.print("\n" + String(therm.getTempCByIndex(0)));
+    }
+  }
+  Serial.print("\n" + String(therm.getTempCByIndex(0)));
+  BTSerial.print("\n" + String(therm.getTempCByIndex(0)));
+  lcd.clear();
+}
+
+void printLCD(String s1, String s2){ // Prints to LCD line by line
+  lcd.setCursor(0,0);
+  lcd.print(s1);
+  lcd.setCursor(0,1);
+  lcd.print(s2);
+}
+
+void reqTemp() {
+  therm.setWaitForConversion(false);
+  therm.requestTemperatures();
+  therm.setWaitForConversion(true);
+}
